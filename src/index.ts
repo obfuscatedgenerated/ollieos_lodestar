c5cd556e054bcb898b061233ee514a58a0cb6293
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-01-06 02:15:21 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import type { Program } from "ollieos/types";
+
+export default {
+    name: "hwpkg",
+    description: "Says hello to the world!",
+    usage_suffix: "",
+    arg_descriptions: {},
+    main: async (data) => {
+        // extract from data to make code less verbose
+        const { term } = data;
+
+        term.writeln("hello package!");
+
+        return 0;
+    }
+} as Program;
